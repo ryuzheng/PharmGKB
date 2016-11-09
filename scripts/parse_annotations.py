2bff61aa00bf1fd4f56b062d5ee3f2649f208740
jen Wilson <jen.wilson521@gmail.com>
2016-11-09 14:57:42 -0800

    an initial parse of relevant annotation data

--- scripts/parse_annotations.py
+++ scripts/parse_annotations.py
@@ -2,14 +2,23 @@
 # written 11-8-16, JLW
 
 import csv,pickle
+from collections import defaultdict
 
 f='../data/annotations/var_drug_ann.tsv'
 dr=csv.DictReader(open(f,'rU'),delimiter='\t')
 
 var_to_drug={}
+gene_var_chem_anno=defaultdict(list)
 for row in dr:
-	rsid=row[]
-	chem=row[]
+	rsid=row['Variant']
+	chem=row['Chemical']
 	sig=row['Significance']
 	gene=row['Gene']
 	sent=row['Sentence']
+	
+	var_to_drug[rsid]=chem
+	gene_dic={'chem':chem,'significance':sig,'sentence':sent,'rsid':rsid}
+	gene_var_chem_anno[gene].append(gene_dic)
+
+pickle.dump(var_to_drug,open('../mapped_dics/var_to_drug.pkl','w'))
+pickle.dump(gene_var_chem_anno,open('../mapped_dics/gene_var_chem_anno.pkl','w'))
